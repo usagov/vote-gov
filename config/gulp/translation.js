4227cc2e130b9cf244b36d4deac4b138879a5a91
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-06-02 13:30:32 -0400

    Create translated URLs from base content;
    This patch adds the ability to generate new content and layout
    directories related specifically to translating. These generated content
    and layout directories are ignored by git and use only during the
    development and build process. These files are necessary to create
    translated URLs and should only be modified via gulp tasks. The current
    `register/` directories in `content/` and `layouts/` are the base files
    that are used to generate the Spanish files.

--- config/gulp/translation.js
+++ config/gulp/translation.js
@@ -4,32 +4,40 @@ var del = require('del');
 var pkg = require('../../package.json');
 var runSequence = require('run-sequence');
 var spawn = require('cross-spawn');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var stateNames = require('./state-names.json');
 
 gulp.task('clean-translation', function () {
+  gutil.log(gutil.colors.cyan('clean-translation'), 'Removing generated register/ files');
   return del([
     './layouts/registrar',
     './content/registrar',
   ]);
 });
 
-gulp.task('copy-content', function (done) {
+gulp.task('copy-content-spanish', function (done) {
 
-  var copyContent = spawn('cp', [
-    '-rvf',
-    './content/register',
-    './content/registrar',
-  ]);
-
-  copyContent.stdout.on('data', function (data) {
-    gutil.log(gutil.colors.blue('copy-content'), '\n' + data);
-  });
-
-  copyContent.on('error', done);
-  copyContent.on('close', done);
-
+  return gulp.src('./content/register/*.md')
+    .pipe(rename(function (path) {
+      var file = stateNames[path.basename].file;
+      path.basename = file;
+    }))
+    .pipe(replace(/title = "(.+)"/, function (match, p1) {
+      var name = p1.replace(/\s/g, '-').replace(/\./g, '').toLowerCase();
+      var title = stateNames[name].title;
+      var file = stateNames[name].file;
+      return (
+        'title = "' + title + '"' +
+        "\n" +
+        'slug = "' + file + '"';
+      );
+    }))
+    .pipe(gulp.dest('./content/registrar'));
 
 });
-gulp.task('copy-layouts', function (done) {
+
+gulp.task('copy-layouts-spanish', function (done) {
 
   var copyLayout = spawn('cp', [
     '-rvf',
@@ -38,7 +46,7 @@ gulp.task('copy-layouts', function (done) {
   ]);
 
   copyLayout.stdout.on('data', function (data) {
-    gutil.log(gutil.colors.blue('copy-layouts'), '\n' + data);
+    gutil.log(gutil.colors.blue('copy-layouts-spanish'), '\n' + data);
   });
 
   copyLayout.on('error', done);
@@ -51,12 +59,12 @@ gulp.task('copy-translation', [ 'clean-translation' ], function (done) {
 
   gutil.log(
     gutil.colors.cyan('copy-translation'),
-    'Copying files from content/ & layouts/'
+    'Copying files from content/ & layouts/ for translated URLs'
   );
 
   runSequence(
-    'copy-content',
-    'copy-layouts',
+    'copy-content-spanish',
+    'copy-layouts-spanish',
     done
   );
 
