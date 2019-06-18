e654b4059a9a479e3af29843140b2e27fc81d3f7
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-06-18 14:39:26 -0400

    remove scsslint job

--- config/gulp/styles.js
+++ config/gulp/styles.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 // var scsslint = require('gulp-scss-lint');
 
 // gulp.task('styles', [ 'scss-lint' ], function () {
-gulp.task('styles', [ 'scss-lint' ], function () {
+gulp.task('styles', function () {
 
   gutil.log(gutil.colors.cyan('styles'), 'Compiling Sass assets');
 
@@ -38,17 +38,17 @@ gulp.task('styles', [ 'scss-lint' ], function () {
 
 });
 
-gulp.task('scss-lint', function (done) {
+// gulp.task('scss-lint', function (done) {
 
-  if (!cFlags.test) {
-    gutil.log(gutil.colors.cyan('scss-lint'), 'Disabling linting');
-    return done();
-  }
+//   if (!cFlags.test) {
+//     gutil.log(gutil.colors.cyan('scss-lint'), 'Disabling linting');
+//     return done();
+//   }
 
-  return gulp.src('./assets/styles/**/*.scss')
-    .pipe(scsslint({
-      config: './.scss-lint.yml',
-    }))
-    .pipe(scsslint.failReporter());
+//   return gulp.src('./assets/styles/**/*.scss')
+//     .pipe(scsslint({
+//       config: './.scss-lint.yml',
+//     }))
+//     .pipe(scsslint.failReporter());
 
-});
+// });
