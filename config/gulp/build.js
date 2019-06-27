cf9287a0ccfc45e36a7f00d971ab458f5d2abad0
vtostado <vanessa.tostado-aguilar@gsa.gov>
2019-06-27 11:22:58 -0400

    seperate english and spanish builds

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -25,20 +25,116 @@ function printPackageInfo (done){
 
 }
 
-function watch () {
-   gutil.log(gutil.colors.cyan('watch'), 'Watching assets for changes');
-   gulp.watch('./assets/styles/**/*.scss', gulp.task( 'styles' ));
-   gulp.watch('./assets/scripts/**/*.js', gulp.task( 'scripts' ));
-   gulp.watch('./assets/images/**/*', gulp.task( 'images' ));
-   gutil.log(gutil.colors.cyan('watch'), 'Watching content & layouts for changes');
-   gulp.watch([
-     './content/register/*.md',
-     './layouts/register/**/*.html'
-   ], gulp.series( 'copy-translation' ) );
-
-}
-
+// function watch () {
+//    gutil.log(gutil.colors.cyan('watch'), 'Watching assets for changes');
+//    gulp.watch('./assets/styles/**/*.scss', gulp.task( 'styles' ));
+//    gulp.watch('./assets/scripts/**/*.js', gulp.task( 'scripts' ));
+//    gulp.watch('./assets/images/**/*', gulp.task( 'images' ));
+//    gutil.log(gutil.colors.cyan('watch'), 'Watching content & layouts for changes');
+//    gulp.watch([
+//      './content/register/*.md',
+//      './layouts/register/**/*.html'
+//    ], gulp.series( 'copy-translation' ) );
 //
+// }
+
+
+function ws (done){
+
+
+      setConfig = process.env.npm_package_config_votegov_hugo_es;
+      setURL = 'http://localhost/es/';
+
+
+    gutil.log(
+      gutil.colors.cyan('website'),
+      'Using environment-specified --config path: ' + setConfig
+    );
+
+    gutil.log(
+      gutil.colors.cyan('website'),
+      'Using environment-specified BaseUrl: ' + setURL
+    );
+
+    done();
+
+    //
+    // var hugo_args = [
+    //   'server',
+    //   '--watch',
+    //   '--buildDrafts',
+    //   '--config=' + setConfig,
+    //   '--baseURL=' + setURL,
+    // ];
+    //
+    // var hugo = spawn('hugo', hugo_args);
+    //
+    // gutil.log(
+    //   gutil.colors.cyan('spawn hugo'),
+    //
+    // );
+    //
+    // hugo.stdout.on('data', function (data) {
+    //   gutil.log(gutil.colors.blue('website'), '\n' + data);
+    // });
+    //
+    // hugo.stderr.on('data', function (data) {
+    //   gutil.log(gutil.colors.red('build:website'), '\n' + data);
+    // });
+    //
+    // hugo.on('error', done);
+    // hugo.on('close', done);
+    //
+
+  }
+
+
+  function we (done){
+
+      var setConfig = process.env.npm_package_config_votegov_hugo_en;
+      var setURL = 'http://localhost/';
+
+
+
+      gutil.log(
+        gutil.colors.cyan('website'),
+        'Using environment-specified --config path: ' + setConfig
+      );
+
+      gutil.log(
+        gutil.colors.cyan('website'),
+        'Using environment-specified BaseUrl: ' + setURL
+      );
+
+
+      var hugo_args = [
+        'server',
+        '--watch',
+        '--buildDrafts',
+        // '--config=' + setConfig,
+        // '--baseURL=' + setURL,
+      ];
+
+      var hugo = spawn('hugo', hugo_args);
+
+      gutil.log(
+        gutil.colors.cyan('spawn hugo'),
+
+      );
+
+      hugo.stdout.on('data', function (data) {
+        gutil.log(gutil.colors.blue('website'), '\n' + data);
+      });
+
+      hugo.stderr.on('data', function (data) {
+        gutil.log(gutil.colors.red('build:website'), '\n' + data);
+      });
+
+      hugo.on('error', done);
+      hugo.on('close', done);
+
+
+    }
 function website (done){
 
     var setConfig = process.env.npm_package_config_votegov_hugo_en;
@@ -89,6 +185,95 @@ function website (done){
 
   }
 
+function buildEnglish(done) {
+  gutil.log(gutil.colors.cyan('build:website'), 'START ENGLISH WEBSITE');
+  gutil.log(gutil.colors.cyan('build:website'), 'Building static website via Hugo');
+
+// English config is default
+var setConfig = process.env.npm_package_config_votegov_hugo_en;
+var setURL = process.env.BASEURL || '';
+
+gutil.log(
+  gutil.colors.cyan('build:website'),
+  'Using environment-specified --config path: ' + setConfig
+);
+
+gutil.log(
+  gutil.colors.cyan('build:website'),
+  'Using environment-specified BaseUrl: ' + setURL
+);
+
+
+if ('development' === process.env.NODE_ENV) {
+
+  var hugo_args = [
+    'server',
+    '--watch',
+    '--buildDrafts',
+    '--config=' + setConfig,
+    '--baseURL=' + setURL,
+  ];
+
+
+  var hugo = spawn('hugo', hugo_args);
+
+  hugo.stdout.on('data', function (data) {
+    gutil.log(gutil.colors.blue('build:website'), '\n' + data);
+  });
+
+  hugo.stderr.on('data', function (data) {
+    gutil.log("gutil.colors.red('build:website'), '\n' + data");
+  });
+
+
+  hugo.on('error', done);
+  hugo.on('close', done);
+}
+done();
+
+
+}
+
+function buildSpanish(done) {
+  gutil.log(gutil.colors.cyan('build:website'), 'BUILD SPANISH SITE');
+  //gutil.log(gutil.colors.cyan('build:website'), 'Building static website via Hugo');
+
+  var setURL = process.env.BASEURL || '';
+  var setConfig = process.env.npm_package_config_votegov_hugo_es;
+
+
+if ('development' === process.env.NODE_ENV) {
+
+  var hugo_args = [
+    'server',
+    '--watch',
+    '--buildDrafts',
+    '--config=' + setConfig,
+    '--baseURL=' + setURL,
+  ];
+
+
+  var hugo = spawn('hugo', hugo_args);
+
+
+  hugo.stdout.on('data', function (data) {
+    gutil.log(gutil.colors.blue('build:website'), '\n' + data);
+  });
+
+  hugo.stderr.on('data', function (data) {
+    gutil.log("gutil.colors.red('build:website'), '\n' + data");
+  });
+
+
+  hugo.on('error', done);
+  hugo.on('close', done);
+}
+done();
+
+
+}
+
+
 function buildWebsite (done) {
 
     gutil.log(gutil.colors.cyan('build:website'), 'START BUILD WEBSITE');
@@ -98,9 +283,9 @@ function buildWebsite (done) {
   var setConfig = process.env.npm_package_config_votegov_hugo_en;
   var setURL = process.env.BASEURL || '';
 
-  if ('spanish' === process.env.NODE_LANG) {
+  // if ('spanish' === process.env.NODE_LANG) {
     setConfig = process.env.npm_package_config_votegov_hugo_es;
-  }
+  // }
 
   gutil.log(
     gutil.colors.cyan('build:website'),
@@ -145,16 +330,27 @@ function buildWebsite (done) {
 
 exports.clean = clean;
 exports.printPackageInfo = printPackageInfo;
-exports.buildWebsite = buildWebsite;
-exports.watch = watch;
+// exports.buildWebsite = buildWebsite;
+exports.buildEnglish = buildEnglish;
+exports.buildSpanish = buildSpanish;
+// exports.watch = watch;
 exports.website= website;
+// exports.we= we;
+// exports.ws= ws;
+
 
 var build = gulp.series(clean, printPackageInfo, gulp.parallel('styles', 'scripts', 'images', 'fonts'), 'copy-translation');
-var buildWebsite = gulp.series (build, buildWebsite);
+// var buildWebsite = gulp.series (build, buildWebsite);
+var buildWebsite = gulp.series (build, gulp.parallel (buildEnglish, buildSpanish));
+// var buildWebsite = gulp.series (build, gulp.parbuildEnglish);
+
+
 // TODO:  Website should run parallel(build, watch), but watch task hangs and does not complete
 var website = gulp.series (build, website);
+// var website = gulp.series (build, ws,we);
+
 // var website = gulp.series (gulp.parallel (build,watch), web);
 gulp.task('build', build);
 gulp.task ('buildWebsite' , buildWebsite);
-gulp.task ('watch' , watch);
+// gulp.task ('watch' , watch);
 gulp.task ('website' , website);
