7b38df4972e4ac3c197d70dc7a5791a8b072fab9
Vanessa <vtostado@wesleyan.edu>
2019-07-11 09:25:13 -0400

    test data

--- config/gulp/data.js
+++ config/gulp/data.js
@@ -1,7 +1,12 @@
 var gulp = require("gulp");
+var gutil = require('gulp-util');
 var jsonSchema = require("gulp-json-schema");
 
 gulp.task("validate", () => {
-  return gulp.src("./data/**/*.json")
-    .pipe(jsonSchema("schema.json"));
+  gutil.log(gutil.colors.cyan('validate'), 'Validating json data');
+  // return gulp.src("./dates/**/*.json")
+  return gulp.src('./lang/spanish/state-names.json')
+
+    .pipe(jsonSchema("dates_schema.json"));
+    console.log("done");
 });
