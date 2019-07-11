cedb418a19cab0026ff0628f943ac84380c84bd5
Vanessa <vtostado@wesleyan.edu>
2019-07-11 11:04:28 -0400

    modify schema, remove id*

--- config/gulp/data.js
+++ config/gulp/data.js
@@ -2,11 +2,14 @@ var gulp = require("gulp");
 var gutil = require('gulp-util');
 var jsonSchema = require("gulp-json-schema");
 
-gulp.task("validate", () => {
-  gutil.log(gutil.colors.cyan('validate'), 'Validating json data');
-  // return gulp.src("./dates/**/*.json")
-  return gulp.src('./lang/spanish/state-names.json')
 
-    .pipe(jsonSchema("dates_schema.json"));
-    console.log("done");
-});
+function validate (){
+gutil.log(gutil.colors.cyan('validate'), 'Validating json data');
+  return gulp.src('./config/gulp/dates/dates.json')
+    .pipe(jsonSchema("./config/gulp/dates/dates_schema.json", {verbose:true, loadMissingSchemas:true}));
+
+};
+
+
+exports.validate = validate;
+gulp.task("validate", validate);
