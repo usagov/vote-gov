8d826abe83c46220a855be34f8375a3f30f10617
Vanessa <vtostado@wesleyan.edu>
2019-07-12 08:02:30 -0400

    replace spanish links if one exists

--- config/gulp/translation.js
+++ config/gulp/translation.js
@@ -3,7 +3,6 @@ var gutil = require('gulp-util');
 var del = require('del');
 var pkg = require('../../package.json');
 var spawn = require('cross-spawn');
-var find = require('gulp-find');
 var rename = require('gulp-rename');
 var replace = require('gulp-replace');
 var spanishStateNames = require('./lang/spanish/state-names.json');
@@ -48,56 +47,39 @@ gulp.task('copy-layouts-spanish', function (done) {
 
 });
 
-// gulp.task('copy-links-spanish', function (done) {
-//   gutil.log(
-//     gutil.colors.cyan('copy-links-spanish'),
-//     'Copying links into state.md files.'
-//   );
-//   for (var state in spanishStateNames) {
-//      fileName = "./content/es/registrar/" + spanishStateNames[state].state_abbreviation + ".md"
-//      populate(fileName, state);
-//    };
-//    done();
-// });
-//
-// function populate(fileName,state ){
-//   return gulp.src(fileName)
-//     .pipe(replace(/external_link = "(.+)"/, function (match,p1) {
-//      var link = spanishStateNames[state].external_link;
-//       return ( 'external_link = "' + link + '"' )
-//     }))
-//     .pipe(gulp.dest('./content/es/registrar'));
-// }
+gulp.task('copy-links-spanish', function (done) {
+  gutil.log(
+    gutil.colors.cyan('copy-links-spanish'),
+    'Copying links into state.md files.'
+  );
+  for (var state in spanishStateNames) {
+    // console.log(state);
+     fileName = "./content/es/registrar/" + spanishStateNames[state].state_abbreviation + ".md"
+     populate(fileName, state);
+   };
+   done();
+});
 
-gulp.task('copy-links-spanish', function () {
-  return gulp.src('./content/es/registrar/*.md')
-    .pipe(replace(/external_link = "(.+)"/, function (match, p1) {
-      console.log(match);
-      var external_link = spanishStateNames[find(/title = "(.+)"/)].external_link;
-      return (
-        'external_link = "' + external_link + '"'
-      );
+function populate(fileName,state ){
+  // console.log(state);
+  return gulp.src(fileName)
+    .pipe(replace(/external_link = "(.+)"/, function (match,p1) {
+      // console.log (fileName, "===>",state, "===>" , link);
+     var link = spanishStateNames[state].external_link;
+     if (link != " " ){
+       // console.log("NO EMPTY", link)
+         return ( 'external_link = "' + link + '"' )
+
+     } else {
+       // console.log(" EMPTY" ,p1 )
+         return ( 'external_link = "' + p1 + '"' )
+
+     }
+
     }))
     .pipe(gulp.dest('./content/es/registrar'));
- });
+}
 
- //here just for my own note
- // gulp.task('populateStates', function () {
- //   //from register
- //    return gulp.src('./content/en/register/*.md')
- //    //look through state file for title line
- //    .pipe(scan({ term: /title = "(.+)"/ } , fn: function (match, file ,p1 ) {
- //    //create title line variable
- //     var stateName = match.replace(/\s/g, '-').replace(/\./g, '').toLowerCase();
- //         }))
- //         //replace register line with new register deadline from electionDates
- //       .pipe(replace(/register = "(.+)"/, function (match,p1) {
- //               var register_deadline = electionDates[stateName].important_dates[0].date;
- //               return ('register = "' + register_deadline + '"');
- //   }}))
- //    //put register stuff back into register folder
- //      .pipe(gulp.dest('./content/en/register'));
- //   }}));
 
 gulp.task('copy-translation', gulp.series( 'clean-translation', 'copy-content-spanish', 'copy-layouts-spanish', 'copy-links-spanish'  , function (done) {
   gutil.log(
