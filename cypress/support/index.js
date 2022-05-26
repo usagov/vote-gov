3abe75de6d46bef400f1d5b5bcfd0244663f451a
Christian Medders <christian.medders@bixal.com>
2022-05-26 11:27:57 -0400

    added command to call site map... will be used in future tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,28 +1,8 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
-
 import './commands'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
 
 Cypress.Commands.add('getUrlsArray', () => {
   cy.request({
-    url: 'http://vote.gov/sitemap.xml',
+    url: 'http://localhost:1313/sitemap.xml',
     headers: {
       'Content-Type': 'text/xml; charset=utf-8',
       'user-agent':
