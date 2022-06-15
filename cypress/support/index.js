f29ce998486bcd33a5fda5316e25eb38cb20dff3
Christian Medders <christian.medders@bixal.com>
2022-06-15 17:30:33 -0400

    added rules for plug ins

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,4 +1,5 @@
 import './commands'
+import 'cypress-axe'
 
 Cypress.Commands.add('getUrlsArray', () => {
   cy.request({
@@ -18,3 +19,24 @@ Cypress.Commands.add('getUrlsArray', () => {
     )
 })
 
+
+
+function terminalLog(violations) {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`
+  )
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length
+    })
+  )
+
+  cy.task('table', violationData)
+}
