2c0f9abcde35d1481e444bf02609f5aa497bcc90
Christian Medders <christian.medders@bixal.com>
2022-06-17 10:18:04 -0400

    took out unused code

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,25 +18,3 @@ Cypress.Commands.add('getUrlsArray', () => {
         .map(el => el.innerText)
     )
 })
-
-
-
-function terminalLog(violations) {
-  cy.task(
-    'log',
-    `${violations.length} accessibility violation${
-      violations.length === 1 ? '' : 's'
-    } ${violations.length === 1 ? 'was' : 'were'} detected`
-  )
-  // pluck specific keys to keep the table readable
-  const violationData = violations.map(
-    ({ id, impact, description, nodes }) => ({
-      id,
-      impact,
-      description,
-      nodes: nodes.length
-    })
-  )
-
-  cy.task('table', violationData)
-}
