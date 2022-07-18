a952a6b5dd6f05347424fd21d23feaf66d3512c1
Christian Medders <christian.medders@bixal.com>
2022-07-18 16:39:46 -0400

    added test for links on state pages

---
+++ cypress/integration/tests/sate-links.spec.js
@@ -0,0 +1,77 @@
+/// <reference types="Cypress" />
+
+describe('check links on stage pages', () => {
+  // it('alaska links check', () => {
+  //   cy.visit('http://localhost:1313/register/ak/')
+  //   cy.get('[data-test="footer-2"]').should('be.visible')
+
+  //   // check links on page
+  //   cy.get('[href="https://voterregistration.alaska.gov/?ref=voteusa_en"]').click()
+  //   cy.url().should('be.equal', 'https://voterregistration.alaska.gov/?ref=voteusa_en')
+  //   cy.go('back')
+
+  //   cy.get('[href="https://www.elections.alaska.gov/Core/voterregistration.php?ref=voteusa_en"]').click()
+  //   cy.url().should('be.equal', 'https://www.elections.alaska.gov/Core/voterregistration.php?ref=voteusa_en')
+  //   cy.go('back')
+
+  //   cy.get('[href="https://myvoterinformation.alaska.gov/?ref=voteusa_en"]').click()
+  //   cy.url().should('be.equal', 'https://myvoterinformation.alaska.gov/?ref=voteusa_en')
+  //   cy.go('back')
+
+  //   cy.get('[data-test="back-button"]').click()
+  //   cy.url().should('be.equal', 'http://localhost:1313/')
+  // })
+
+  // it('american samoa links check', () => {
+  //   cy.visit('http://localhost:1313/register/as/')
+  //   cy.get('[data-test="footer-2"]').should('be.visible')
+
+  //   // check links on page
+  //   cy.get('[href="https://aselectionoffice.gov/?ref=voteusa_en"]').then(links =>{cy.get(links[1])
+  //   .click()
+  //   cy.url().should('be.equal', 'https://aselectionoffice.gov/?ref=voteusa_en')
+  //   cy.go('back')
+  // })
+    
+  //   cy.get('[href="https://aselectionoffice.gov/status.php?ref=voteusa_en"]').click()
+  //   cy.url().should('be.equal', 'https://aselectionoffice.gov/status.php?ref=voteusa_en')
+  //   cy.go('back')
+
+  //   cy.get('[data-test="back-button"]').click()
+  //   cy.url().should('be.equal', 'http://localhost:1313/')
+  // })
+
+  it('arkansas links check', () => {
+    cy.visit('http://localhost:1313/register/ar/')
+    cy.get('[data-test="footer-2"]').should('be.visible')
+
+    // check links on page
+    cy.get('[href="https://www.sos.arkansas.gov/elections/voter-information/?ref=voteusa_en"]').click()
+    cy.url().should('be.equal', 'https://www.sos.arkansas.gov/elections/voter-information/?ref=voteusa_en')
+    cy.go('back')
+
+    cy.get('[href="https://www.voterview.ar-nova.org/voterview?ref=voteusa_en"]').click()
+    cy.url().should('be.equal', 'https://www.voterview.ar-nova.org/voterview?ref=voteusa_en')
+    cy.go('back')
+
+    cy.get('[data-test="back-button"]').click()
+    cy.url().should('be.equal', 'http://localhost:1313/')
+  })
+
+  it('north dakota links check', () => {
+    cy.visit('http://localhost:1313/register/nd/')
+    cy.get('[data-test="footer-2"]').should('be.visible')
+
+  //   // check links on page
+  //   cy.get('[href="https://vip.sos.nd.gov/PortalList.aspx?ref=voteusa_en"]').then(links =>{cy.get(links[1])
+  //   .click()
+  //   cy.url().should('be.equal', 'https://vip.sos.nd.gov/PortalList.aspx?ref=voteusa_en')
+  //   cy.go('back')
+  // })
+
+  cy.get('[href="https://vip.sos.nd.gov/PortalList.aspx?ref=voteusa_en"]').click({ multiple: true })
+
+    cy.get('[data-test="back-button"]').click()
+    cy.url().should('be.equal', 'http://localhost:1313/')
+  })
+})
