18e5d951540c4130e9cf9dbc2b39c052bf371e42
Christian Medders <christian.medders@bixal.com>
2022-07-18 16:39:35 -0400

    updated test

--- cypress/integration/tests/home-page.spec.js
+++ cypress/integration/tests/home-page.spec.js
@@ -9,60 +9,52 @@ describe('check homepage', () => {
     cy.get('[data-test="footer-2"]').should('be.visible')
   })
 
-  // it('check language switching on homepage', () => {
-  //   // check spanish 
-  //   cy.get('[data-test="language-switcher"]').select("/es/")
-  //   cy.get('[data-test="main-header"]').should('contain', 'Inscríbase para votar')
+  it('check language switching on homepage', () => {
+    // check spanish 
+    cy.get('[data-test="language-switcher"]').select("/es/")
+    cy.get('[data-test="main-header"]').should('contain', 'Inscríbase para votar')
 
-  //   // check tagalog
-  //   cy.get('[data-test="language-switcher"]').select("/tl/")
-  //   cy.get('[data-test="main-header"]').should('contain', 'Magrehistro para bumoto')
+    // check tagalog
+    cy.get('[data-test="language-switcher"]').select("/tl/")
+    cy.get('[data-test="main-header"]').should('contain', 'Magrehistro para bumoto')
 
-  //   // check vietnamese
-  //   cy.get('[data-test="language-switcher"]').select("/vi/")
-  //   cy.get('[data-test="main-header"]').should('contain', 'Đăng kí bỏ phiếu bầu')
+    // check vietnamese
+    cy.get('[data-test="language-switcher"]').select("/vi/")
+    cy.get('[data-test="main-header"]').should('contain', 'Đăng kí bỏ phiếu bầu')
 
-  //   // check hindi
-  //   cy.get('[data-test="language-switcher"]').select("/hi/")
-  //   cy.get('[data-test="main-header"]').should('contain', 'मतदान करने के लिए पंजीकरण करें')
+    // check hindi
+    cy.get('[data-test="language-switcher"]').select("/hi/")
+    cy.get('[data-test="main-header"]').should('contain', 'मतदान करने के लिए पंजीकरण करें')
 
-  //   // check bengali
-  //   cy.get('[data-test="language-switcher"]').select("/bn/")
-  //   cy.get('[data-test="main-header"]').should('contain', 'ভোট দিতে নিবন্ধন করুন')
+    // check bengali
+    cy.get('[data-test="language-switcher"]').select("/bn/")
+    cy.get('[data-test="main-header"]').should('contain', 'ভোট দিতে নিবন্ধন করুন')
 
-  //   // check khmer
-  //   cy.get('[data-test="language-switcher"]').select("/km/")
-  //   cy.get('[data-test="main-header"]').should('contain','ចុះឈ្មោះដើម្បីបោះឆ្នោត')
+    // check khmer
+    cy.get('[data-test="language-switcher"]').select("/km/")
+    cy.get('[data-test="main-header"]').should('contain','ចុះឈ្មោះដើម្បីបោះឆ្នោត')
 
-  //    // check kkorean
-  //    cy.get('[data-test="language-switcher"]').select("/ko/")
-  //    cy.get('[data-test="main-header"]').should('contain','유권자 등록하기')
+     // check kkorean
+     cy.get('[data-test="language-switcher"]').select("/ko/")
+     cy.get('[data-test="main-header"]').should('contain','유권자 등록하기')
     
-  // })
+  })
 
-  // it('check links on homepage', () => {
-  //   cy.get('[href="https://www.usa.gov/voting"]').click()
-  //   cy.url().should('be.equal', 'https://www.usa.gov/voting')
-  //   cy.go('back')
+  it('check links on homepage', () => {
+    cy.get('[href="https://www.usa.gov/voting"]').click()
+    cy.url().should('be.equal', 'https://www.usa.gov/voting')
+    cy.go('back')
 
-  //   cy.get('[href="https://www.usa.gov/election-day"]').click()
-  //   cy.url().should('be.equal', 'https://www.usa.gov/election-day')
-  //   cy.go('back')
+    cy.get('[href="https://www.usa.gov/election-day"]').click()
+    cy.url().should('be.equal', 'https://www.usa.gov/election-day')
+    cy.go('back')
 
-  //   cy.get('[href="https://www.usa.gov/voter-id"]').click()
-  //   cy.url().should('be.equal', 'https://www.usa.gov/voter-id')
-  //   cy.go('back')
+    cy.get('[href="https://www.usa.gov/voter-id"]').click()
+    cy.url().should('be.equal', 'https://www.usa.gov/voter-id')
+    cy.go('back')
 
-  //   cy.get('[href="https://www.usa.gov/election"]').click()
-  //   cy.url().should('be.equal', 'https://www.usa.gov/election')
-  //   cy.go('back')
-  // })
-
-  it('check footer links', () => {
-    cy.get('[data-test="footer-1"]')
-    cy.find('[class="usagov-img"]')
+    cy.get('[href="https://www.usa.gov/election"]').click()
+    cy.url().should('be.equal', 'https://www.usa.gov/election')
+    cy.go('back')
   })
 })
-
-
-// cy.url().should('be.equal', '')
