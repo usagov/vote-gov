9d00240c52f705e2280e645456d7df8897a30a55
Ray Estrada <ray.estrada@bixal.com>
2022-08-02 09:21:40 -0700

    Vote 299/improve performance (#574)
    * VOTE-299 Improve mobile performance
    
    Co-authored-by: mlloydbixal <mandy.lloyd@bixal.com>

--- assets/scripts/start.js
+++ assets/scripts/start.js
@@ -1,3 +1,4 @@
-require('uswds');
+require('../../node_modules/@uswds/uswds/dist/js/uswds-init.min.js');
+require('../../node_modules/@uswds/uswds/dist/js/uswds.min.js');
 
 // Custom site JS goes here, and is browserify'ed by `gulp script`
--- gulp/images.js
+++ gulp/images.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+const webp = require('gulp-webp');
 var log = require('fancy-log');
 var colors = require('ansi-colors');
 
@@ -7,8 +8,8 @@ gulp.task('images', function () {
   log(colors.cyan('images'), 'Copying image assets');
   var stream = gulp.src([
     './assets/img/**/*',
-    './node_modules/uswds/src/img/**/*',
-  ]);
+    './node_modules/@uswds/uswds/dist/img/**/*',
+  ]).pipe(webp());
 
   return stream.pipe(gulp.dest('./static/assets/img'));
 
