6d2e20dc0d0358cc08e7344a450ac72e841f0656
Christian Medders <christian.medders@bixal.com>
2022-08-19 15:13:42 -0400

    updated test with new format and contat footer

--- cypress/e2e/tests/footer.cy.js
+++ cypress/e2e/tests/footer.cy.js
@@ -1,48 +1,25 @@
 /// <reference types="Cypress" />
 
 describe('Check footer on site',() => {
-  beforeEach('visit page', () => {
+  before('visit page', () => {
     cy.visit('localhost:1313')
   })
 
+  it("validate that contact footer is present", () =>{
+    cy.get('[data-test="footer-contact"]').should('be.visible')
+  })
+
   it("validate that each link is working as expected", () =>{
-    cy.get('[data-test="footer"]').should('be.visible')
-
-    // Ensure that links are taking user to correct page.
-    cy.get('[href="https://www.gsa.gov/"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsa.gov/about-us"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/about-us')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsa.gov/website-information/accessibility-aids"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/website-information/accessibility-aids')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsa.gov/reference/freedom-of-information-act-foia"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/reference/freedom-of-information-act-foia')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsa.gov/reference/civil-rights-programs/notification-and-federal-employee-antidiscrimination-and-retaliation-act-of-2002"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/reference/civil-rights-programs/notification-and-federal-employee-antidiscrimination-and-retaliation-act-of-2002')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsaig.gov/"]').click()
-    cy.url().should('be.equal', 'https://www.gsaig.gov/')
-    cy.go('back')
-
-    cy.get('[href="https://www.gsa.gov/reference/reports/budget-performance"]').click()
-    cy.url().should('be.equal', 'https://www.gsa.gov/reference/reports/budget-performance')
-    cy.go('back')
-
-    cy.get('[href="https://www.usa.gov/policies"]').click()
-    cy.url().should('be.equal', 'https://www.usa.gov/policies')
-    cy.go('back')
-
-    cy.get('[href="https://usa.gov"]').click()
-    cy.url().should('be.equal', 'https://www.usa.gov/')
-    cy.go('back')
+    cy.get('[data-test="footer"]').should('be.visible').find('a').then(link => {
+      cy.get(link[0]).contains('General Services Administration').should('have.attr', 'href').and('include', 'https://www.gsa.gov/')
+      cy.get(link[1]).contains('About Vote.gov').should('have.attr', 'href').and('include', 'https://www.gsa.gov/about-us')
+      cy.get(link[2]).contains('Accessibility').should('have.attr', 'href').and('include', 'https://www.gsa.gov/website-information/accessibility-aids')
+      cy.get(link[3]).contains('FOIA Requests').should('have.attr', 'href').and('include', 'https://www.gsa.gov/reference/freedom-of-information-act-foia')
+      cy.get(link[4]).contains('No Fear Act data').should('have.attr', 'href').and('include', 'https://www.gsa.gov/reference/civil-rights-programs/notification-and-federal-employee-antidiscrimination-and-retaliation-act-of-2002')
+      cy.get(link[5]).contains('Office of the Inspector General').should('have.attr', 'href').and('include', 'https://www.gsaig.gov/')
+      cy.get(link[6]).contains('Performance Reports').should('have.attr', 'href').and('include', 'https://www.gsa.gov/reference/reports/budget-performance')
+      cy.get(link[7]).contains('Privacy Policy').should('have.attr', 'href').and('include', 'https://www.usa.gov/policies')
+      cy.get(link[8]).contains('Visit USA.gov').should('have.attr', 'href').and('include', 'https://usa.gov')
+    })
   })
 })
