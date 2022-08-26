41d76b7d1ef11d253cd9a07688e4687c9343d340
Christian Medders - Bixal <88721460+clmedders@users.noreply.github.com>
2022-08-26 18:15:59 -0400

    Update link validator test to exclude problematic links

--- cypress/e2e/link-validator/link-validator.cy.js
+++ cypress/e2e/link-validator/link-validator.cy.js
@@ -1,12 +1,24 @@
 /// <reference types="Cypress" />
 
 const allPages = require("../../fixtures/state-page.json");
+// The following urls have been validated manually but will not be able to run through this cypress test due to permission issues.
+const excludedHrefs = [
+  // Connecticut links.
+  '[href="https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_en"]',
+  // Ohio links.
+  '[href="https://olvr.ohiosos.gov/?ref=voteusa_en"]',
+  '[href="https://voterlookup.ohiosos.gov/voterlookup.aspx?ref=voteusa_en"]',
+  // Rhode Island links.
+  '[href="https://vote.sos.ri.gov/Home/RegistertoVote?ref=voteusa_en"]',
+  '[href="https://vote.sos.ri.gov/Voter/RegisterToVote?ref=voteusa_en"]',
+  '[href="https://vote.sos.ri.gov/Home/UpdateVoterRecord?ActiveFlag=0&?ref=voteusa_en"]'
+];
 
 describe("Link Validator Test", () => {
   const baseURL = Cypress.env("base_url")
     ? Cypress.env("base_url")
     : "http://localhost:1313";
- 
+
   const singlePage =
     Cypress.env("name") && Cypress.env("route")
       ? [
@@ -32,8 +44,12 @@ describe("Link Validator Test", () => {
         cy.visit({
           url: baseURL + page.route,
         });
-
-        cy.get("a:not([href*='mailto:'])").each(link => {
+        cy.get("a:not([href*='mailto:']")
+          // The following urls have been validated manually but will not be able to run through this cypress test due to permission issues.
+        .not(excludedHrefs[0])
+        .not(excludedHrefs[1]).not(excludedHrefs[2])
+        .not(excludedHrefs[3]).not(excludedHrefs[4]).not(excludedHrefs[5])
+        .each(link => {
                 cy.request(link.prop('href')).then(link => {
                   expect(link.status).to.eq(200)
                 })
