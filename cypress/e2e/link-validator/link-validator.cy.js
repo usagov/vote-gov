bdfb1f5bac888a99b90d45ab1d0fecda040a5589
Ray Estrada <ray.estrada@bixal.com>
2022-08-26 15:11:09 -0700

    minor adjustment to excluded link hrefs

--- cypress/e2e/link-validator/link-validator.cy.js
+++ cypress/e2e/link-validator/link-validator.cy.js
@@ -1,19 +1,24 @@
 /// <reference types="Cypress" />
 
 const allPages = require("../../fixtures/state-page.json");
-// the following urls have been validated manually but will not be able to run through this cypress test due to permission issues
-const ctUrl = ('[href="https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_en"]')
-const ohUrl0 = ('[href="https://olvr.ohiosos.gov/?ref=voteusa_en"]')
-const ohUrl1 = ('[href="https://voterlookup.ohiosos.gov/voterlookup.aspx?ref=voteusa_en"]')
-const riUrl0 = ('[href="https://vote.sos.ri.gov/Home/RegistertoVote?ref=voteusa_en"]')
-const riUrl1 = ('[href="https://vote.sos.ri.gov/Voter/RegisterToVote?ref=voteusa_en"]')
-const riUrl2 = ('[href="https://vote.sos.ri.gov/Home/UpdateVoterRecord?ActiveFlag=0&?ref=voteusa_en"]')
+// The following urls have been validated manually but will not be able to run through this cypress test due to permission issues.
+const excludedHrefs = [
+  // Connecticut links.
+  '[href="https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_en"]',
+  // Ohio links.
+  '[href="https://olvr.ohiosos.gov/?ref=voteusa_en"]',
+  '[href="https://voterlookup.ohiosos.gov/voterlookup.aspx?ref=voteusa_en"]',
+  // Rhode Island links.
+  '[href="https://vote.sos.ri.gov/Home/RegistertoVote?ref=voteusa_en"]',
+  '[href="https://vote.sos.ri.gov/Voter/RegisterToVote?ref=voteusa_en"]',
+  '[href="https://vote.sos.ri.gov/Home/UpdateVoterRecord?ActiveFlag=0&?ref=voteusa_en"]'
+];
 
 describe("Link Validator Test", () => {
   const baseURL = Cypress.env("base_url")
     ? Cypress.env("base_url")
     : "http://localhost:1313";
- 
+
   const singlePage =
     Cypress.env("name") && Cypress.env("route")
       ? [
@@ -40,10 +45,10 @@ describe("Link Validator Test", () => {
           url: baseURL + page.route,
         });
         cy.get("a:not([href*='mailto:']")
-        // the following urls have been validated manually but will not be able to run through this cypress test due to permission issues
-        .not(ctUrl)
-        .not(ohUrl0).not(ohUrl1)
-        .not(riUrl0).not(riUrl1).not(riUrl2)
+          // The following urls have been validated manually but will not be able to run through this cypress test due to permission issues.
+        .not(excludedHrefs[0])
+        .not(excludedHrefs[1]).not(excludedHrefs[2])
+        .not(excludedHrefs[3]).not(excludedHrefs[4]).not(excludedHrefs[5])
         .each(link => {
                 cy.request(link.prop('href')).then(link => {
                   expect(link.status).to.eq(200)
