f3264779a08f6f0938974b336392722498f8c50c
Christian Medders <christian.medders@bixal.com>
2022-08-26 16:14:37 -0400

    added urls to be skipped in test

--- cypress/e2e/link-validator/link-validator.cy.js
+++ cypress/e2e/link-validator/link-validator.cy.js
@@ -1,6 +1,13 @@
 /// <reference types="Cypress" />
 
 const allPages = require("../../fixtures/state-page.json");
+// the following urls have been validated manually but will not be able to run through this cypress test due to permission issues
+const ctUrl = ('[href="https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_en"]')
+const ohUrl0 = ('[href="https://olvr.ohiosos.gov/?ref=voteusa_en"]')
+const ohUrl1 = ('[href="https://voterlookup.ohiosos.gov/voterlookup.aspx?ref=voteusa_en"]')
+const riUrl0 = ('[href="https://vote.sos.ri.gov/Home/RegistertoVote?ref=voteusa_en"]')
+const riUrl1 = ('[href="https://vote.sos.ri.gov/Voter/RegisterToVote?ref=voteusa_en"]')
+const riUrl2 = ('[href="https://vote.sos.ri.gov/Home/UpdateVoterRecord?ActiveFlag=0&?ref=voteusa_en"]')
 
 describe("Link Validator Test", () => {
   const baseURL = Cypress.env("base_url")
@@ -32,8 +39,12 @@ describe("Link Validator Test", () => {
         cy.visit({
           url: baseURL + page.route,
         });
-
-        cy.get("a:not([href*='mailto:'])").each(link => {
+        cy.get("a:not([href*='mailto:']")
+        // the following urls have been validated manually but will not be able to run through this cypress test due to permission issues
+        .not(ctUrl)
+        .not(ohUrl0).not(ohUrl1)
+        .not(riUrl0).not(riUrl1).not(riUrl2)
+        .each(link => {
                 cy.request(link.prop('href')).then(link => {
                   expect(link.status).to.eq(200)
                 })
