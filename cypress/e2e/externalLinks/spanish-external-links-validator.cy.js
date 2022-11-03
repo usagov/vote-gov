f972a829da86f6c176d7d486df405aa430080178
Ray Estrada <raymond.estrada@gsa.gov>
2022-11-02 17:02:21 -0700

    VOTE-644 exclude links from external links test

--- cypress/e2e/externalLinks/spanish-external-links-validator.cy.js
+++ cypress/e2e/externalLinks/spanish-external-links-validator.cy.js
@@ -2,6 +2,13 @@
 
 const allPages = require("../../fixtures/spanish-site.json");
 
+const excludedlinks = [
+  'https://voterregistration.ct.gov/OLVR/welcome.do?ref=voteusa_en',
+  'https://voterservices.elections.maryland.gov/OnlineVoterRegistration/InstructionsStep1?ref=voteusa_en',
+  'https://www.sec.state.ma.us/ovr/?ref=voteusa_en',
+  'https://olvr.ohiosos.gov/?ref=voteusa_en'
+];
+
 describe("External Link Validator Test", () => {
   const baseURL = Cypress.env("base_url")
     ? Cypress.env("base_url")
@@ -32,10 +39,15 @@ describe("External Link Validator Test", () => {
         cy.visit({
           url: baseURL + page.route,
         });
-        cy.get("a[href^='https://']").each(link => {
-          cy.request(link.prop('href')).then(link => {
-            expect(link.status).to.eq(200)
-          })
+        cy.get("div[role='main'] a[href^='https://']").each(link => {
+          if (excludedlinks.indexOf(link.prop('href')) == -1) {
+            cy.request({
+              url: link.prop('href'),
+              failOnStatusCode: false
+            }).then((response) => {
+              expect(response.status).to.oneOf([200])
+            })
+          }
         })
       }
     );
