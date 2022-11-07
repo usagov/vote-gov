fdaafecc955c650b3ef4ee4e04fab90a5df164bc
Mandy Lloyd <amanda.lloyd@gsa.gov>
2022-11-07 10:49:32 -0500

    moves uswds init script

--- assets/scripts/start.js
+++ assets/scripts/start.js
@@ -1,4 +1,3 @@
-require('../../node_modules/@uswds/uswds/dist/js/uswds-init.min.js');
 require('../../node_modules/@uswds/uswds/dist/js/uswds.min.js');
 
 // Custom site JS goes here, and is browserify'ed by `gulp script`
