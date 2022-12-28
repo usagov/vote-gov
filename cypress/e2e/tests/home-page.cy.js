f737e3b063bdbde4fa295e5772653758cbf70d9e
Christian Medders <christian.medders@bixal.com>
2022-12-28 09:03:03 -0500

    took home page lanuage switcher test out of this test

--- cypress/e2e/tests/home-page.cy.js
+++ cypress/e2e/tests/home-page.cy.js
@@ -6,38 +6,7 @@ describe('Test vote.gov homepage', () => {
   it('Confirm homepage load', () => {
     cy.get('[data-test="footer"]').should('be.visible')
   })
-  it('Validate language switching on homepage', () => {
-    // Test Spanish.
-    cy.get('[data-test="language-switcher"]').select("/es/")
-    cy.get('[data-test="main-header"]').should('contain', 'Inscríbase para votar')
-    // Test Bengali.
-    cy.get('[data-test="language-switcher"]').select("/bn/")
-    cy.get('[data-test="main-header"]').should('contain', 'ভোট দিতে নিবন্ধন করুন')
-    // Test simplified Chinese.
-    cy.get('[data-test="language-switcher"]').select("/zh-hans/")
-    cy.get('[data-test="main-header"]').should('contain','登记投票')
-    // Test traditional Chinese.
-    cy.get('[data-test="language-switcher"]').select("/zh/")
-    cy.get('[data-test="main-header"]').should('contain','登記投票')
-    // Test Hindi.
-    cy.get('[data-test="language-switcher"]').select("/hi/")
-    cy.get('[data-test="main-header"]').should('contain', 'मतदान करने के लिए पंजीकरण करें')
-    // Test Khmer.
-    cy.get('[data-test="language-switcher"]').select("/km/")
-    cy.get('[data-test="main-header"]').should('contain','ចុះឈ្មោះដើម្បីបោះឆ្នោត')
-    // Test Korean.
-    cy.get('[data-test="language-switcher"]').select("/ko/")
-    cy.get('[data-test="main-header"]').should('contain','유권자 등록하기')
-    // Test Tagalog.
-    cy.get('[data-test="language-switcher"]').select("/tl/")
-    cy.get('[data-test="main-header"]').should('contain', 'Magrehistro para bumoto')
-    // Test Vietnamese.
-    cy.get('[data-test="language-switcher"]').select("/vi/")
-    cy.get('[data-test="main-header"]').should('contain', 'Đăng ký bỏ phiếu bầu')
-    // Test Yup'ik.
-    cy.get('[data-test="language-switcher"]').select("/ypk/")
-    cy.get('[data-test="main-header"]').should('contain','Ilaten Nakmikiyaghqaa')
-  })
+  
   it('Check accordion function on homepage', () => {
     cy.get('[data-test="accordion-button"]').each(accordionButton => {
       cy.get(accordionButton).click()
