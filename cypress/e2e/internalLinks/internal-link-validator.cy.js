d2bf740d1cfa8ee369434d285b61ffe85f61edbc
Christian Medders - Bixal <88721460+clmedders@users.noreply.github.com>
2023-01-09 19:41:31 -0500

    VOTE-790: Update internal links test to run though the site pages json and not sitemap (#795)

--- cypress/e2e/internalLinks/internal-link-validator.cy.js
+++ cypress/e2e/internalLinks/internal-link-validator.cy.js
@@ -1,18 +1,47 @@
 /// <reference types="Cypress" />
 
-describe("Internal Link Validator Test", () => {
-it('Validate internal links', () => {
-  cy.getUrlsEnglish().then($urls => {
-    $urls.forEach(url => {
-      cy.visit(url).then(() => {
+const allPages = require("../../fixtures/site-pages.json");
+
+describe("External Link Validator Test", () => {
+  const baseURL = Cypress.env("base_url")
+    ? Cypress.env("base_url")
+    : "http://localhost:1313";
+
+  const singlePage =
+    Cypress.env("name") && Cypress.env("route")
+      ? [
+          {
+            name: Cypress.env("name"),
+            route: Cypress.env("route"),
+          },
+        ]
+      : null;
+  const pages = singlePage !== null ? singlePage : allPages;
+  pages.forEach((page) => {
+    it( 
+      `${page.name === "" ? "home" : page.name}`,
+      () =>
+        Cypress.env("retries") === true
+          ? {
+              retries: {
+                runMode: 2,
+              },
+            }
+          : {},
+      () => {
+        cy.visit({
+          url: baseURL + page.route,
+        });
         cy.get("a[href^='/']").each(link => {
-          cy.request(link.prop('href')).then(link => {
-            expect(link.status).to.eq(200)
-          })
+            cy.request({
+              url: link.prop('href'),
+              failOnStatusCode: false
+            }).then((response) => {
+              expect(response.status).to.eq(200)
+            })
         })
-    })
-  })
-})
-})
+      }
+    );
+  });    
 });
 
