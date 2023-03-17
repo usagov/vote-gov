3f946516641a3b8e5ba75402c9144f60b0a3cad5
mlloydbixal <amanda.lloyd@gsa.gov>
2023-03-17 17:07:17 -0400

    Ensures <svg> elements with an img, graphics-document or graphics-symbol role have an accessible text

--- assets/scripts/external-links.js
+++ assets/scripts/external-links.js
@@ -28,7 +28,7 @@
     if (externalLinks.length) {
       for (let i = 0; i < externalLinks.length; i++) {
         if (!matchesWhitelistedDomain(externalLinks[i].href)) {
-          const svg = `<svg aria-hidden="true" viewBox="3 3 18 18" width="10" height="10"><path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" fill="currentColor"></path></svg>`;
+          const svg = `<svg aria-hidden="true" viewBox="3 3 18 18" width="10" height="10" role="graphics-symbol" aria-labelledby="external icon"><path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" fill="currentColor"></path></svg>`;
           var svgIcon = `<span class="ext-link">&#xFEFF;${svg}</span>`;
           externalLinks[i].setAttribute('target', '_blank');
           externalLinks[i].insertAdjacentHTML( 'beforeend', svgIcon );
