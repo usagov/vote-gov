03ab94d8d6ddaff167c4e964f029c0ff1ec15a37
mlloydbixal <amanda.lloyd@gsa.gov>
2023-04-06 16:27:23 -0400

    fix ext-link

--- assets/scripts/external-links.js
+++ assets/scripts/external-links.js
@@ -5,32 +5,16 @@
   (() => {
 
     // Enable external links behavior.
-    const externalLinks = document.querySelectorAll('a[href^="http"]');
-    // Set all whitelisted domains.
-    // const whitelistedDomains = [
-    // ];
-
-    // // Checks if a value matches one of the whitelistedDomains.
-    // const matchesWhitelistedDomain = (value) => {
-    //   for (let i = 0; i < whitelistedDomains.length; i++) {
-    //     if (value.indexOf(whitelistedDomains[i]) != -1) {
-    //       return true;
-    //     }
-    //   }
-
-    //   return false;
-    // }
+    const externalLinks = document.querySelectorAll('a[href^="https"]');
 
     // Process external links.
     if (externalLinks.length) {
       for (let i = 0; i < externalLinks.length; i++) {
-        // if (!matchesWhitelistedDomain(externalLinks[i].href)) {
           const svg = `<svg aria-hidden="true" viewBox="3 3 18 18" width="10" height="10"><path d="M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" fill="currentColor"></path></svg>`;
           var svgIcon = `<span class="ext-link">&#xFEFF;${svg}</span>`;
           externalLinks[i].setAttribute('target', '_blank');
           externalLinks[i].insertAdjacentHTML( 'beforeend', svgIcon );
           externalLinks[i].setAttribute('title', 'External link opens new window');
-        // }
       }
     }
 
