07f5920db8a58db9e4ebcb7fbcc2f4b944ece001
Ray Estrada <raymond.estrada@gsa.gov>
2023-11-28 11:00:30 -0800

    test update

--- gulp/nvrf.js
+++ gulp/nvrf.js
@@ -2,13 +2,31 @@ var gulp = require('gulp');
 var log = require('fancy-log');
 var colors = require('ansi-colors');
 
-gulp.task('nvrf', function () {
-
-  log(colors.cyan('nvrf'), 'Copying nvrf assets');
+gulp.task('nvrf-assets', function () {
   var stream = gulp.src([
-    './node_modules/vote-gov-nvrf-app/dist/**/*'
+    './node_modules/vote-gov-nvrf-app/dist/assets/*'
   ]);
 
-  return stream.pipe(gulp.dest('./static/nvrf'));
+  return stream.pipe(gulp.dest('./static/assets/nvrf/assets'));
 
 });
+
+gulp.task('nvrf-files', function () {
+  var stream = gulp.src([
+    './node_modules/vote-gov-nvrf-app/dist/files/*'
+  ]);
+
+  return stream.pipe(gulp.dest('./static/assets/nvrf/files'));
+
+});
+
+gulp.task('nvrf-data', function () {
+  var stream = gulp.src([
+    './node_modules/vote-gov-nvrf-app/dist/data/en/*'
+  ]);
+
+  return stream.pipe(gulp.dest('./content/en/nvrf-test/data/en'));
+
+});
+
+gulp.task('nvrf', gulp.series('nvrf-assets', 'nvrf-files', 'nvrf-data'));
