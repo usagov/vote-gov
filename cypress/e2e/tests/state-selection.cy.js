67b6a73ba6112bd7d8072f9a57f640d9ee44e854
Christian Medders - Bixal <88721460+clmedders@users.noreply.github.com>
2023-11-30 19:34:11 -0500

    VOTE-536:  Update and re-enable state selector test (#944)

--- cypress/e2e/tests/state-selection.cy.js
+++ cypress/e2e/tests/state-selection.cy.js
@@ -1,32 +1,60 @@
 /// <reference types="Cypress" />
 
-// describe('Validate state selection options', () => {
-//   beforeEach('visit page', () => {
-//     cy.visit('localhost:1313')
-//   })
-//
-//   it('Test Alaska', () => {
-//     cy.get('[data-test="state-selection"]').select('Alaska')
-//     cy.get('[data-test="register-button"]').click()
-//     cy.url().should('include', '/register/ak/')
-//   })
-//
-//   it('Test American Samoa', () => {
-//     cy.get('[data-test="state-selection"]').select('American Samoa')
-//     cy.get('[data-test="register-button"]').click()
-//     cy.url().should('include', '/register/as/')
-//   })
-//
-//   it('Test Arkansas', () => {
-//     cy.get('[data-test="state-selection"]').select('Arkansas')
-//     cy.get('[data-test="register-button"]').click()
-//     cy.url().should('include', '/register/ar/')
-//   })
-//
-//   it('Test North Dakota', () => {
-//     cy.get('[data-test="state-selection"]').select('North Dakota')
-//     cy.get('[data-test="register-button"]').click()
-//     cy.url().should('include', '/register/nd/')
-//   })
-//
-// })
+describe('Validate state selection options', () => {
+  beforeEach('visit page', () => {
+    cy.visit('localhost:1313')
+  })
+
+  it('Verify that menu drop down is working', () => {
+    cy.get('[data-test="dropdown-btn"]').click()
+    cy.get('[data-test="state-list"]').should('be.visible')
+  })
+
+  it('Verify that type search is working', () => {
+    // * Check that 'South' renders expected states
+    cy.get('[data-test="state-selection"]').type('South')
+    cy.get('[data-test="state-list"]').should('contain', 'South Carolina', 'South Dakota')
+    cy.reload()
+    // * Check that 'Virgin' renders expected states
+    cy.get('[data-test="state-selection"]').type('Virgin')
+    cy.get('[data-test="state-list"]').should('contain', 'U.S. Virgin Island', 'Virginia', 'West Virginia')
+    cy.reload()
+    // * Check that 'Carolina' renders expected states
+    cy.get('[data-test="state-selection"]').type('Carolina')
+    cy.get('[data-test="state-list"]').should('contain', 'South Carolina', 'North Carolina')
+    cy.reload()
+    // * Check that 'mar' renders expected states
+    cy.get('[data-test="state-selection"]').type('mar')
+    cy.get('[data-test="state-list"]').should('contain', 'Maryland', 'Northern Mariana Islands')
+  })
+
+  it('Verify that user can navigate to state page', () => {
+    // * Check Alaska
+    cy.get('[data-test="state-selection"]').type('Alaska')
+    cy.get('[data-test=state-list]').find('li').then(option => {
+      cy.get(option[1]).click()
+      cy.url().should('include', '/register/ak/')
+    })
+    cy.get('[data-test="vote-logo"]').click()
+    // * Check American Samoa
+    cy.get('[data-test="state-selection"]').type('American Samoa')
+    cy.get('[data-test=state-list]').find('li').then(option => {
+      cy.get(option[2]).click()
+      cy.url().should('include', '/register/as/')
+    })
+    cy.get('[data-test="vote-logo"]').click()
+    // * Check Arkansas
+    cy.get('[data-test="state-selection"]').type('Arkansas')
+    cy.get('[data-test=state-list]').find('li').then(option => {
+      cy.get(option[4]).click()
+      cy.url().should('include', '/register/ar/')
+    })
+    cy.get('[data-test="vote-logo"]').click()
+    // * Check North Dakota
+    cy.get('[data-test="state-selection"]').type('North Dakota')
+    cy.get('[data-test=state-list]').find('li').then(option => {
+      cy.get(option[36]).click()
+      cy.url().should('include', '/register/nd/')
+    })  
+  })
+})
